cf7dbc81427bdde275a1166d296738ff69c8c9bb
Konrad Borowski <konrad@borowski.pw>
2021-06-20 16:54:15 +0200

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,40 @@
+let
+  moz_overlay = import (builtins.fetchTarball "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+with pkgs;
+mkShell {
+  packages = [
+    dotnet-sdk_5
+    ghc
+    perl
+    php
+    rakudo
+    sqlite
+
+    # C/C++
+    clang
+    gcc
+
+    # Go
+    go
+    goimports
+
+    # Node.js
+    nodejs
+    nodePackages.typescript
+
+    # Python
+    black
+    python
+    
+    # Rust
+    cargo
+    clippy
+    rustfmt
+  ];
+  JAVA8 = openjdk8;
+  JAVA11 = openjdk11;
+  RUST_BETA = rustChannels.beta.rust;
+  RUST_NIGHTLY = rustChannels.nightly.rust;
+}
---
+++ src/main.rs
@@ -0,0 +1,109 @@
+use rocket::serde::json::Json;
+use rocket::{launch, post, routes};
+use serde::{Deserialize, Serialize};
+use std::ffi::OsString;
+use std::io;
+use std::process::Stdio;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::process::Command;
+use tokio::time::{timeout, Duration};
+use tokio::{fs, try_join};
+
+#[derive(Deserialize)]
+struct Input {
+    files: Vec<File>,
+    stdin: String,
+    code: String,
+}
+
+#[derive(Deserialize)]
+struct File {
+    name: String,
+    contents: String,
+}
+
+#[derive(Serialize)]
+struct Output {
+    status: Option<i32>,
+    stdout: String,
+    stderr: String,
+}
+
+async fn read_limited(f: impl AsyncReadExt + Unpin, out: &mut Vec<u8>) -> io::Result<()> {
+    f.take(1_000_000).read_to_end(out).await?;
+    Ok(())
+}
+
+#[post("/", data = "<input>")]
+async fn sandbox(input: Json<Input>) -> io::Result<Json<Output>> {
+    let home = tempfile::tempdir()?;
+    for File { name, contents } in &input.files {
+        fs::write(home.path().join(name), contents).await?;
+    }
+    let mut private = OsString::from("--private=");
+    private.push(home.path());
+    let mut child = Command::new("/run/wrappers/bin/firejail")
+        .arg(private)
+        .args(&[
+            "--caps.drop=all",
+            "--net=none",
+            "--no3d",
+            "--nodbus",
+            "--nodvd",
+            "--nogroups",
+            "--nonewprivs",
+            "--noprofile",
+            "--noroot",
+            "--nosound",
+            "--noautopulse",
+            "--novideo",
+            "--nou2f",
+            "--private-dev",
+            "--private-tmp",
+            "--rlimit-cpu=10",
+            "--rlimit-fsize=10000000",
+            "--rlimit-nofile=192",
+            "--rlimit-nproc=2048",
+            "--seccomp",
+            "--shell=none",
+            "--x11=none",
+            "--quiet",
+            "sh",
+            "-c",
+            &input.code,
+        ])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .kill_on_drop(true)
+        .spawn()?;
+    let mut stdin = child.stdin.take().expect("stdin");
+    let stdout = child.stdout.take().expect("stdout");
+    let stderr = child.stderr.take().expect("stderr");
+    let mut stdout_out = Vec::new();
+    let mut stderr_out = Vec::new();
+    let result = timeout(Duration::from_secs(20), async {
+        let ((), (), (), status) = try_join!(
+            stdin.write_all(input.stdin.as_bytes()),
+            read_limited(stdout, &mut stdout_out),
+            read_limited(stderr, &mut stderr_out),
+            child.wait(),
+        )?;
+        io::Result::Ok(status.code())
+    })
+    .await;
+    let status = match result {
+        Err(_) => None, // Elapsed, not an error
+        Ok(status) => status?,
+    };
+    Ok(Json(Output {
+        stdout: String::from_utf8_lossy(&stdout_out).into_owned(),
+        stderr: String::from_utf8_lossy(&stderr_out).into_owned(),
+        status,
+    }))
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![sandbox])
+}
