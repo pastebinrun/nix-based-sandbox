7d5bfe09219154fa1b2fc45852818ce362b3a0eb
Konrad Borowski <konrad@borowski.pw>
2023-10-18 18:29:04 +0200

    Comply with REUSE recommendations

--- shell.nix
+++ shell.nix
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2023 Konrad Borowski <konrad@borowski.pw>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 let
   moz_overlay = import (builtins.fetchTarball "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
   pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Konrad Borowski <konrad@borowski.pw>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 use rocket::serde::json::Json;
 use rocket::{launch, post, routes};
 use serde::{Deserialize, Serialize};
